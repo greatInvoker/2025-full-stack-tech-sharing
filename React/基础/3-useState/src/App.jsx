5ffffef456badb6e9af80b39852c6fcac638969f
invoker <593597559@qq.com>
2025-06-09 17:35:12 +0800

    chore: 动态读取版本号

--- React/基础/3-useState/src/App.jsx
+++ React/基础/3-useState/src/App.jsx
@@ -1,10 +1,11 @@
 import Table from "./table";
+import { version } from "react";
 
 function App() {
 	return (
 		<div id="container">
 			<h1>ReactHook-useState</h1>
-			<p>react@18.2.0</p>
+			<p>react@{version}</p>
 			<Table />
 		</div>
 	);
